95558cf5ad298f1cfc1d3f743186006873d8764a
khoi <khoiracle@gmail.com>
2019-04-09 17:17:42 +0700

    fmt

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"flag"
-  "fmt"
+	"fmt"
+	"net/http"
 	"os"
-  "net/http"
-  "time"
-  "sync"
+	"sync"
+	"time"
 )
 
 type responseMeta struct {
-  statusCode int
-  contentLength int64
-  responseTime time.Duration
+	statusCode    int
+	contentLength int64
+	responseTime  time.Duration
 }
 
 func main() {
@@ -26,58 +26,57 @@ func main() {
 		os.Exit(1)
 	}
 
-
 	host := flag.Arg(0)
-  fmt.Printf("🚀 Benchmarking %v (be patient)...\n", host)
+	fmt.Printf("🚀 Benchmarking %v (be patient)...\n", host)
 
-  tasks := make(chan struct{})
-  var wg sync.WaitGroup
-  var result []*responseMeta
-  var resultMux sync.Mutex
+	tasks := make(chan struct{})
+	var wg sync.WaitGroup
+	var result []*responseMeta
+	var resultMux sync.Mutex
 
-  for worker := 0; worker < *concurrency; worker++ {
-    wg.Add(1)
-    go func() {
-      defer wg.Done()
-      for i := range tasks {
-        fmt.Println(i)
-        resultMux.Lock()
-        result = append(result, get(host))
-        resultMux.Unlock()
-      }
-    }()
-  }
+	for worker := 0; worker < *concurrency; worker++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := range tasks {
+				fmt.Println(i)
+				resultMux.Lock()
+				result = append(result, get(host))
+				resultMux.Unlock()
+			}
+		}()
+	}
 
-  for i:=0; i < *requestNums; i++ {
-    tasks <- struct{}{}   
-  }
+	for i := 0; i < *requestNums; i++ {
+		tasks <- struct{}{}
+	}
 
-  close(tasks)
-  wg.Wait()
+	close(tasks)
+	wg.Wait()
 
-  var totalTime time.Duration
+	var totalTime time.Duration
 
-  for _, v:= range result {
-    totalTime += v.responseTime
-  }
+	for _, v := range result {
+		totalTime += v.responseTime
+	}
 
-  mean := int(totalTime / time.Millisecond) / len(result)
+	mean := int(totalTime/time.Millisecond) / len(result)
 
-  fmt.Printf("Time per request: %v [ms] (mean)\n", mean)
+	fmt.Printf("Time per request: %v [ms] (mean)\n", mean)
 }
 
 func get(url string) *responseMeta {
-  start := time.Now()
+	start := time.Now()
 
-  res, err := http.Get(url);
+	res, err := http.Get(url)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  return &responseMeta {
-    statusCode: res.StatusCode,
-    contentLength: res.ContentLength,
-    responseTime: time.Now().Sub(start),
-  }
+	return &responseMeta{
+		statusCode:    res.StatusCode,
+		contentLength: res.ContentLength,
+		responseTime:  time.Now().Sub(start),
+	}
 }
